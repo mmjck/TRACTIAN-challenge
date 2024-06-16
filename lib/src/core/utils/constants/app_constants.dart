4c74f9beb6166c907d484f4d10807966974a5727
mmjck <msantosjck@gmail.com>
2024-06-15 21:17:52 -0400

    feat: added app constants file

---
+++ lib/src/core/utils/constants/app_constants.dart
@@ -0,0 +1,7 @@
+// non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names
+
+class AppConstants {
+  static String LOCATIONS = "locations";
+  static String ASSETS = "assets";
+}
