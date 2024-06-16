3a928b0a27808cc1769cc31942e80c1698b5af0b
mmjck <msantosjck@gmail.com>
2024-06-15 21:18:33 -0400

    feat: strings as key

---
+++ lib/src/core/utils/constants/keys_strings.dart
@@ -0,0 +1,14 @@
+class KeysString {
+  static String apex = "Apex Unit";
+  static String tobias = "Tobias Unit";
+  static String jaguar = "Jaguar Unit";
+
+  static String tractian = 'TRACTIAN';
+
+  static String assets = 'Assets';
+
+  static String energy = "Sensor de Energia";
+  static String critical = "Critico";
+
+  static String search = 'Buscar Ativo ou Local';
+}
