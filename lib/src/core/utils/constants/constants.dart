3fef8f064d3f8db1dfc936d980ee717910220080
mmjck <msantosjck@gmail.com>
2024-06-15 21:18:11 -0400

    feat: assets constants

---
+++ lib/src/core/utils/constants/constants.dart
@@ -0,0 +1,5 @@
+class StaticAssets {
+  static const String imgAssets = "assets/images/assets.png";
+  static const String imgLocation = "assets/images/location.png";
+  static const String imgComponents = "assets/images/components.png";
+}
