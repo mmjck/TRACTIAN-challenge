d6b301982893746dfe5ace7bc3d4f8b344ba2887
mmjck <msantosjck@gmail.com>
2024-06-15 21:22:40 -0400

    feat: added home viewmodel

---
+++ lib/src/features/home/presentation/pages/home_view_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tree_view_application/src/features/details/di/details_dependency_injection.dart';
+import 'package:tree_view_application/src/features/details/presentation/pages/details_view.dart';
+import 'package:tree_view_application/src/features/details/presentation/pages/details_view_model.dart';
+import 'package:tree_view_application/src/shared/models/base_view_model.dart';
+
+class HomeViewModel extends BaseViewModel {
+  BuildContext? context;
+  void setContext(BuildContext ctx) {
+    context = ctx;
+  }
+
+  void navigateToDetailsScreen({required Map<String, dynamic> database}) {
+    if (context != null) {
+      Navigator.push(
+        context!,
+        MaterialPageRoute(
+            builder: (c) => InfoDetailsView(
+                viewModel: DetailsViewModel(
+                    dependencies: DetailsDepencendies(database: database)))),
+      );
+    }
+  }
+
+  @override
+  void onInit() {}
+}
