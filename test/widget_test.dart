1916b16f4a9d96043623634a5ec2004f7d14125e
mmjck <msantosjck@gmail.com>
2024-06-15 21:18:53 -0400

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:tree_view_application/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Tractian());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
